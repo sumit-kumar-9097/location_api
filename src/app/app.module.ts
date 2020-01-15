77f33e9c12a724da32f200a79dd21a69097a79a6
jhasaurabh312 <jhasaurabh.312@gmail.com>
2020-01-15 18:38:57 +0530

    location api check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationComponent } from './location/location.component';
+import { SafePipe } from './pipe/safe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/location/location.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-location',
+  templateUrl: './location.component.html',
+  styleUrls: ['./location.component.css']
+})
+export class LocationComponent implements OnInit {
+  res: any;
+  constructor(private http : HttpClient) { }
+
+  ngOnInit() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    }
+
+    function showPosition(position) {
+      const latlon = position.coords.latitude + "," + position.coords.longitude;    
+      localStorage.setItem('data',latlon);
+    }  
+    this.location();
+    setTimeout(()=>{
+      this.ngOnInit();
+    },5000);
+  }
+
+  location(){
+    const url = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDm9MO7inWEy-_k6hgApQgCToE1D-AldfY&q='+localStorage.getItem('data') ;
+    console.log(url);
+    return url;
+  }
+
+  search(){
+    let x = (<HTMLInputElement>(document.getElementById('search'))).value
+    console.log(x);
+    
+    const headers = new HttpHeaders({
+          'Content-Type': 'Application/json',     
+    })  
+    
+    return this.http.get('https://us1.locationiq.com/v1/search.php?key=3199bc66a77470&q='+x+'&format=json', {headers:headers}).subscribe((suc) => {
+        this.res=suc;
+        console.log(this.res);
+        const data = this.res.lat + ',' + this.res.lon;
+        localStorage.setItem('data',data);
+        this.location();
+
+    })
+  }
+    
+
+    
+      // const apiKey = 'AIzaSyDm9MO7inWEy-_k6hgApQgCToE1D-AldfY'
+      // console.log(apiKey);
+    
+   
+  
+
+
+  
+
+
+}
